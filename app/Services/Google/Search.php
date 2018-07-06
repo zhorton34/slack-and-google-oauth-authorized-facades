e6e544dc67b6f78c8494b837c39a7d5253c8146b
Zak Horton <zhorton999@gmail.com>
2018-07-06 01:21:31 -0500

    #5 - small refactor

--- app/Services/Google/Search.php
+++ app/Services/Google/Search.php
@@ -3,8 +3,25 @@
 class Search extends GoogleAction
 {
 
-    protected $filters = ['folder', 'document', 'spreadsheet', 'script'];
+    protected $resources = ['folder', 'document', 'spreadsheet', 'script'];
 
+
+    /**
+     * @param $resource
+     *   any method that matches a value in the resources array will search the user
+     *   google drive for all files of that resource type
+     * @param $options
+     * @return \Illuminate\Support\Collection|string
+     */
+    public function __call($resource, $options)
+    {
+        $resource = $this->checkForResource($resource);
+
+        if(!$resource) return 'Can not search for resource that does not exist';
+
+        return $this->find($resource);
+
+    }
 
     protected function find($resource)
     {
@@ -19,21 +36,13 @@ class Search extends GoogleAction
     }
 
 
-    public function __call($resource, $options)
-    {
-        $resource = $this->checkForResource($resource);
 
-        if(!$resource) return 'Can not search for resource that does not exist';
-
-        return $this->find($resource);
-
-    }
 
     protected function checkForResource($resource)
     {
         $resource = rtrim($resource, 's');
 
-        if(in_array($resource, $this->filters)) return $resource;
+        if(in_array($resource, $this->resources)) return $resource;
 
         return false;
 
